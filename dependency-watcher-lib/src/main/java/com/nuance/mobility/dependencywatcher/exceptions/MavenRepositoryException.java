760995efe24a93e969b23208cbf04550552299c9
Dehbozorgi <mahya_dehbozorgi@nuance.com>
2017-07-12 10:29:16 -0400

    add artifact Retriever

---
+++ dependency-watcher-lib/src/main/java/com/nuance/mobility/dependencywatcher/exceptions/MavenRepositoryException.java
@@ -0,0 +1,31 @@
+package com.nuance.mobility.dependencywatcher.exceptions;
+
+public class MavenRepositoryException extends Exception{
+
+	public MavenRepositoryException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public MavenRepositoryException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MavenRepositoryException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MavenRepositoryException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MavenRepositoryException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
