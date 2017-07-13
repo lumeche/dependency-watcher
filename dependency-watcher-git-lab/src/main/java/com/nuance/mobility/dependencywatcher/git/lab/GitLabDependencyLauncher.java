b49107943d928ff63b17ab8c469bc28af9695199
Tobon <Luis_Tobon@nuance.com>
2017-07-13 10:38:57 -0400

    Adding documentation and support for triggering multple branches

--- dependency-watcher-git-lab/src/main/java/com/nuance/mobility/dependencywatcher/git/lab/GitLabDependencyLauncher.java
+++ dependency-watcher-git-lab/src/main/java/com/nuance/mobility/dependencywatcher/git/lab/GitLabDependencyLauncher.java
@@ -2,12 +2,15 @@ package com.nuance.mobility.dependencywatcher.git.lab;
 
 import java.io.IOException;
 import java.util.Collection;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 import javax.annotation.PostConstruct;
 
 import org.gitlab.api.GitlabAPI;
+import org.gitlab.api.models.GitlabBranch;
 import org.gitlab.api.models.GitlabBuild;
 import org.gitlab.api.models.GitlabProject;
 import org.slf4j.Logger;
@@ -21,31 +24,35 @@ import com.nuance.mobility.dependencywatcher.interfaces.IDependencyLauncher;
 public class GitLabDependencyLauncher implements IDependencyLauncher {
 
 	private Logger logger = LoggerFactory.getLogger(GitLabDependencyLauncher.class);;
-	
+
 	@Value("${git.service.account.token}")
 	private String gitServiceAccountToken;
-	
+
 	@Value("${git.url}")
 	private String gitServiceAccountUrl;
-	
+
 	@Value("${git.project.information.regex}")
 	private String gitProjectInformationRegex;
+
+	@Value("${git.project.branches.regex}")
+	private String gitProjectBranchesRegex;
 
 	private GitlabAPI api;
 
 	private Pattern pattern;
 
 	public GitLabDependencyLauncher() {
-		
+
 	}
+
 	@PostConstruct
-	public void init(){
+	public void init() {
 		logger.debug("git account token {}", gitServiceAccountToken);
 		logger.debug("git url{}", gitServiceAccountUrl);
 		logger.debug("git project regex {}", gitProjectInformationRegex);
 		api = GitlabAPI.connect(gitServiceAccountUrl, gitServiceAccountToken);
 		pattern = Pattern.compile(gitProjectInformationRegex);
-		
+
 	}
 
 	@Override
@@ -55,9 +62,7 @@ public class GitLabDependencyLauncher implements IDependencyLauncher {
 		}
 
 	}
-	//FIXME: The match is failing all the time
-	//TODO: The pom file needs to be parsed properly with a library to be able to get the group and version from parents for both the project itself and the dependencies
-	
+
 	private void launchBuild(Artifact dependency) throws DependencyLauncherException {
 		Matcher matcher = pattern.matcher(dependency.getScm());
 		if (!matcher.matches()) {
@@ -66,24 +71,36 @@ public class GitLabDependencyLauncher implements IDependencyLauncher {
 			logger.error(message);
 			throw new DependencyLauncherException(message);
 		}
-		
+
 		String namespace = matcher.group(1);
 		String projectName = matcher.group(2);
 
 		try {
 			GitlabProject project = api.getProject(namespace, projectName);
-			
-			//TODO: Find a way to launch on several branches and not only in the default one
-			String requestPath = String.format("/projects/%s/pipeline?ref=%s", project.getId(),
-					project.getDefaultBranch());
-			GitlabBuild build = api.dispatch().to(requestPath, GitlabBuild.class);
-
-			logger.info("Build started with ID: {}", build.getId());
-		} catch (IOException e) {
+			Stream<String> branches = getTriggerBranch(project);
+			branches.forEach(b -> sendTrigger(project, b));
+		} catch (IOException | RuntimeException e) {
 			logger.error("Error while trying to launch build on project with scm {}", dependency.getScm());
 			throw new DependencyLauncherException(String.format("Error launching git project %s", dependency.getScm()),
 					e);
 		}
+	}
+
+	private void sendTrigger(GitlabProject project, String branch) {
+		String requestPath = String.format("/projects/%s/pipeline?ref=%s", project.getId(), branch);
+		GitlabBuild build;
+		try {
+			build = api.dispatch().to(requestPath, GitlabBuild.class);
+			logger.info("Build started with ID: {}", build.getId());
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private Stream<String> getTriggerBranch(GitlabProject project) throws IOException {
+		List<GitlabBranch> branches = api.getBranches(project);
+		return branches.parallelStream().map(b -> b.getName())
+				.filter(Pattern.compile(gitProjectBranchesRegex).asPredicate());
 
 	}
 
@@ -99,11 +116,11 @@ public class GitLabDependencyLauncher implements IDependencyLauncher {
 		this.gitProjectInformationRegex = gitProjectInformationRegex;
 	}
 
-	
+	public void setGitProjectBranchesRegex(String gitProjectBranchesRegex) {
+		this.gitProjectBranchesRegex = gitProjectBranchesRegex;
+	}
 	void setApi(GitlabAPI api) {
 		this.api = api;
 	}
-	
-	
 
 }
--- dependency-watcher-git-lab/src/test/java/com/nuance/mobility/dependencywatcher/git/lab/GitLabDependencyLauncherTest.java
+++ dependency-watcher-git-lab/src/test/java/com/nuance/mobility/dependencywatcher/git/lab/GitLabDependencyLauncherTest.java
@@ -3,13 +3,16 @@ package com.nuance.mobility.dependencywatcher.git.lab;
 import static org.mockito.Matchers.anyObject;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.gitlab.api.GitlabAPI;
 import org.gitlab.api.http.GitlabHTTPRequestor;
+import org.gitlab.api.models.GitlabBranch;
 import org.gitlab.api.models.GitlabBuild;
 import org.gitlab.api.models.GitlabProject;
 import org.junit.Before;
@@ -22,7 +25,8 @@ import com.nuance.mobility.dependencywatcher.git.lab.GitLabDependencyLauncher;
 
 public class GitLabDependencyLauncherTest {
 
-	private static final String REGEX = "^.*:(.*)/([\\w_-]*).git$";
+	private static final String PROJECT_REGEX = "^.*:(.*)/([\\w_-]*).git$";
+	private static final String BRANCH_REGEX = "master.*";
 	private static final String TOKEN = "sssssss";
 	private static final String GIT_URL = "https://localhost:8082";
 	private GitLabDependencyLauncher testee;
@@ -37,13 +41,23 @@ public class GitLabDependencyLauncherTest {
 		project=mock(GitlabProject.class);
 		httpRequester=mock(GitlabHTTPRequestor.class);
 		artifact=new Artifact("1", "1", "1","git@git.labs.nuance.com:mobility-ncs-components/location-language-metadata-service.git");
-		testee.setGitProjectInformationRegex(REGEX);
+		GitlabBranch master=new GitlabBranch();
+		GitlabBranch masterA=new GitlabBranch();
+		GitlabBranch other=new GitlabBranch();
+		master.setName("master");
+		masterA.setName("masterA");
+		other.setName("other");
+		
+		testee.setGitProjectInformationRegex(PROJECT_REGEX);
+		testee.setGitProjectBranchesRegex(BRANCH_REGEX);
 		testee.setGitServiceAccountToken(TOKEN);
 		testee.setGitServiceAccountUrl(GIT_URL);		
 		testee.init();
 		testee.setApi(api);
 		
 		when(api.getProject(anyString(),anyString())).thenReturn(project);
+		
+		when(api.getBranches(project)).thenReturn(new ArrayList<GitlabBranch>(Arrays.asList(master,masterA,other)));
 		when(api.dispatch()).thenReturn(httpRequester);
 		when(httpRequester.to(anyString(),anyObject())).thenReturn(mock(GitlabBuild.class));
 	}
@@ -51,7 +65,7 @@ public class GitLabDependencyLauncherTest {
 	@Test
 	public void testRegexParsing() throws DependencyLauncherException {		
 		testee.launchDependency(Arrays.asList(artifact));
-		verify(api).dispatch();
+		verify(api,times(2)).dispatch();
 	}
 
 }
